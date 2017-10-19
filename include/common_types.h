1d83247c311b33307311dd6189f0b880e79b2e3e
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2017-10-19 11:24:42 -0400

    Initial commit with basic project structure

---
+++ include/common_types.h
@@ -0,0 +1,14 @@
+//
+// Created by david on 19/10/17.
+//
+
+#ifndef RESERVATION_SYSTEM_COMMON_TYPES_H
+#define RESERVATION_SYSTEM_COMMON_TYPES_H
+
+struct Demand_t {
+    char* Name;
+    char Section;
+    __uint8_t Table;
+};
+
+#endif //RESERVATION_SYSTEM_COMMON_TYPES_H
---
+++ server/source/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <common_types.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
